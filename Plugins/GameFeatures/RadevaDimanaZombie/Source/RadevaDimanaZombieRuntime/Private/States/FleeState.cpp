79fe6dfb6488ca0c6f1da9e33bdd6c47e3e5b754
Dimana Radeva <dimana.radeva@student.howest.be>
2026-06-01 01:41:28 +0200

    Reworked Flee state to use path following instead of steering, and handling the purge zones

--- Plugins/GameFeatures/RadevaDimanaZombie/Source/RadevaDimanaZombieRuntime/Private/States/FleeState.cpp
+++ Plugins/GameFeatures/RadevaDimanaZombie/Source/RadevaDimanaZombieRuntime/Private/States/FleeState.cpp
@@ -1,41 +1,178 @@
 #include "States/FleeState.h"
 #include "Survivor/SurvivorPawn.h"
 #include "SteeringBehaviors.h"
+#include "Village/House/House.h"
+#include "Common/InventoryComponent.h"
+#include "States/WanderState.h"
 
-FleeState::FleeState(ASurvivorPawn* InPawn, AgentMemory* InMemory)
+FleeState::FleeState(ASurvivorPawn* InPawn, AgentMemory* InMemory, WanderState* InWander, CollectState* InCollect)
 {
-	Pawn = InPawn;
-	Memory = InMemory;
+    Pawn = InPawn;
+    Memory = InMemory;
+    Wander = InWander;
+    Collect = InCollect;
 }
 
 void FleeState::OnEnter()
 {
-	if (Pawn)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Green, TEXT("Entering Flee State"));
-	}
+    bHasTarget = false;
+    CurrentPath.Empty();
+    CurrentPathIndex = 0;
+    GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Green, TEXT("Entering Flee State"));
 }
 
 void FleeState::OnExit()
 {
-	if (Pawn)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Red, TEXT("Exiting Flee State"));
-	}
+    bHasTarget = false;
+    CurrentPath.Empty();
+    CurrentPathIndex = 0;
+    GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Red, TEXT("Exiting Flee State"));
+}
+
+void FleeState::BuildPathTo(FVector Target)
+{
+    NavTarget = Target;
+    bHasTarget = true;
+    CurrentPath.Empty();
+    CurrentPathIndex = 0;
+
+    UNavigationPath* Path = UNavigationSystemV1::FindPathToLocationSynchronously(Pawn->GetWorld(), Pawn->GetActorLocation(), NavTarget);
+
+    if (Path && Path->IsValid())
+    {
+        CurrentPath = Path->PathPoints;
+    }
+}
+
+void FleeState::PickFleeTarget()
+{
+    UNavigationSystemV1* NavSystem = UNavigationSystemV1::GetCurrent(Pawn->GetWorld());
+    if (!NavSystem)
+    {
+        return;
+    }
+
+    const TArray<FPerceivedTarget>& PurgeZones = Memory->GetPurgeZones();
+    //purge zone
+    if (PurgeZones.Num() > 0)
+    {
+        GEngine->AddOnScreenDebugMessage(51, 2.f, FColor::Purple, TEXT("Purge zone block entered"));
+
+        int ClosestIndex = 0;
+        float ClosestDist = FVector::Dist2D(Pawn->GetActorLocation(), PurgeZones[0].Location);
+
+        for (int i = 1; i < PurgeZones.Num(); i++)
+        {
+            float Dist = FVector::Dist2D(Pawn->GetActorLocation(), PurgeZones[i].Location);
+            if (Dist < ClosestDist)
+            {
+                ClosestDist = Dist;
+                ClosestIndex = i;
+            }
+        }
+
+        FVector ZoneCenter = PurgeZones[ClosestIndex].Location;
+        float Radius = 50.f;
+
+        FVector ThroughDirection = (ZoneCenter - Pawn->GetActorLocation()).GetSafeNormal();
+
+        FVector ExitPoint = ZoneCenter + ThroughDirection * (Radius + 100.f);
+
+        FNavLocation Result;
+        if (NavSystem->GetRandomReachablePointInRadius(ExitPoint, 1000.f, Result))
+        {
+            BuildPathTo(Result.Location);
+            return;
+        }
+    }
+
+    AActor* UnvisitedHouse = Memory->GetClosestHouse(Pawn->GetActorLocation(), true);
+    if (UnvisitedHouse && IsInsideHouse(UnvisitedHouse))
+    {
+        if (!Collect->IsInventoryFull())
+        {
+            Memory->MarkHouseVisited(UnvisitedHouse);
+        }
+    }
+    else if (UnvisitedHouse && !IsInsideHouse(UnvisitedHouse))
+    {
+        BuildPathTo(UnvisitedHouse->GetActorLocation());
+        return;
+    }
+
+    FVector SpiralPoint = Wander->GetCurrentSpiralPoint();
+    FNavLocation Result;
+    if (NavSystem->GetRandomReachablePointInRadius(SpiralPoint, 500.f, Result))
+    {
+        BuildPathTo(Result.Location);
+    }
+}
+
+bool FleeState::IsInsideHouse(AActor* House) const
+{
+    if (!House)
+    {
+        return false;
+    }
+
+    AHouse* HouseActor = Cast<AHouse>(House);
+    if (!HouseActor)
+    {
+        return false;
+    }
+
+    FHouseBounds Bounds = HouseActor->GetBounds();
+    FVector PawnLoc = Pawn->GetActorLocation();
+
+    return PawnLoc.X >= Bounds.Origin.X - Bounds.Extent.X && PawnLoc.X <= Bounds.Origin.X + Bounds.Extent.X &&
+           PawnLoc.Y >= Bounds.Origin.Y - Bounds.Extent.Y && PawnLoc.Y <= Bounds.Origin.Y + Bounds.Extent.Y;
+}
+
+void FleeState::FollowPath()
+{
+    if (CurrentPathIndex < CurrentPath.Num() - 1)
+    {
+        if (FVector::Dist2D(Pawn->GetActorLocation(), CurrentPath[CurrentPathIndex]) < WaypointAcceptanceRadius)
+        {
+            CurrentPathIndex++;
+        }
+    }
+
+    if (CurrentPath.Num() > 0 && CurrentPathIndex < CurrentPath.Num())
+    {
+        FVector Direction = CurrentPath[CurrentPathIndex] - Pawn->GetActorLocation();
+        Direction.Z = 0.f;
+        Direction.Normalize();
+        Pawn->AddMovementInput(Direction, 1.f);
+    }
+    else
+    {
+        FVector Direction = NavTarget - Pawn->GetActorLocation();
+        Direction.Z = 0.f;
+        Direction.Normalize();
+        Pawn->AddMovementInput(Direction, 1.f);
+    }
 }
 
 void FleeState::Update(float DeltaTime)
 {
-	if (!Pawn || !Memory)
-	{
-		return;
-	}
+    if (!Pawn || !Memory)
+    {
+        return;
+    }
 
-	FVector ThreatLocation = Memory->GetClosestZombieLocation(Pawn->GetActorLocation());
+    int CurrentPurgeCount = Memory->GetPurgeZones().Num();
 
-	FVector Direction = FleeBehavior.CalculateSteering(Pawn, ThreatLocation);
-	Direction.Z = 0.f;
-	Direction = Direction.GetSafeNormal();
+    if (!bHasTarget || FVector::Dist2D(Pawn->GetActorLocation(), NavTarget) < AcceptanceRadius || CurrentPurgeCount > LastPurgeZoneCount)
+    {
+        LastPurgeZoneCount = CurrentPurgeCount;
+        PickFleeTarget();
+    }
 
-	Pawn->AddMovementInput(Direction, 0.4f);
+    if (!bHasTarget)
+    {
+        return;
+    }
+
+    FollowPath();
 }
--- Plugins/GameFeatures/RadevaDimanaZombie/Source/RadevaDimanaZombieRuntime/Public/States/FleeState.h
+++ Plugins/GameFeatures/RadevaDimanaZombie/Source/RadevaDimanaZombieRuntime/Public/States/FleeState.h
@@ -2,20 +2,38 @@
 #include "../AgentFSM.h"
 #include "../AgentMemory.h"
 #include "SteeringBehaviors.h"
+#include "NavigationSystem.h"
+#include "NavigationPath.h"
+#include "States/CollectState.h"
 
 class ASurvivorPawn;
-class Flee;
+class WanderState;
 
 class FleeState : public State
 {
 public:
-	FleeState(ASurvivorPawn* InPawn, AgentMemory* InMemory);
-	void OnEnter() override;
-	void OnExit() override;
-	void Update(float DeltaTime) override;
+    FleeState(ASurvivorPawn* InPawn, AgentMemory* InMemory, WanderState* InWander, CollectState* InCollect);
+    void OnEnter() override;
+    void OnExit() override;
+    void Update(float DeltaTime) override;
 private:
-	ASurvivorPawn* Pawn = nullptr;
-	AgentMemory* Memory = nullptr;
+    void BuildPathTo(FVector Target);
+    void PickFleeTarget();
+    bool IsInsideHouse(AActor* House) const;
+    void FollowPath();
 
-	Flee FleeBehavior;
+    ASurvivorPawn* Pawn = nullptr;
+    AgentMemory* Memory = nullptr;
+    WanderState* Wander = nullptr;
+    CollectState* Collect = nullptr;
+
+    FVector NavTarget;
+    TArray<FVector> CurrentPath;
+    int CurrentPathIndex = 0;
+    bool bHasTarget = false;
+
+    float AcceptanceRadius = 50.f;
+    float WaypointAcceptanceRadius = 50.f;
+
+    int LastPurgeZoneCount = 0;
 };
