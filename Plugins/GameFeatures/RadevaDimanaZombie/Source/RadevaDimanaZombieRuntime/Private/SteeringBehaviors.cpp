da5f05f9c5ff60ad386a4c8e9bdd9ea1f44c2c54
Dimana Radeva <dimana.radeva@student.howest.be>
2026-05-31 21:24:59 +0200

    Removed commented out code

--- Plugins/GameFeatures/RadevaDimanaZombie/Source/RadevaDimanaZombieRuntime/Private/SteeringBehaviors.cpp
+++ Plugins/GameFeatures/RadevaDimanaZombie/Source/RadevaDimanaZombieRuntime/Private/SteeringBehaviors.cpp
@@ -1,25 +1,6 @@
 #include "SteeringBehaviors.h"
 #include "Survivor/SurvivorPawn.h"
 
-//FVector Wander::CalculateSteering(ASurvivorPawn* Pawn, float DeltaTime)
-//{
-//    if (!Pawn)
-//    {
-//    	return FVector::ZeroVector;
-//    }
-//
-//    WanderAngle += FMath::FRandRange(-WanderJitter, WanderJitter) * DeltaTime;
-//
-//    FVector CircleCenter = Pawn->GetActorLocation() + Pawn->GetActorForwardVector() * CircleDistance;
-//    FVector WanderPoint = CircleCenter + FVector(FMath::Cos(WanderAngle), FMath::Sin(WanderAngle), 0.f) * CircleRadius;
-//
-//    FVector Direction = WanderPoint - Pawn->GetActorLocation();
-//    Direction.Z = 0.f;
-//    Direction.Normalize();
-//
-//    return Direction;
-//}
-
 FVector Seek::CalculateSteering(ASurvivorPawn* Pawn, FVector TargetLocation)
 {
 	FVector Direction = TargetLocation - Pawn->GetActorLocation();
@@ -27,12 +8,3 @@ FVector Seek::CalculateSteering(ASurvivorPawn* Pawn, FVector TargetLocation)
 	Direction.Normalize();
 	return Direction;
 }
-
-FVector Flee::CalculateSteering(ASurvivorPawn* Pawn, FVector ThreatLocation)
-{
-	FVector Direction = Pawn->GetActorLocation() - ThreatLocation;
-	Direction.Z = 0.f;
-	Direction.Normalize();
-	return Direction;
-}
-
--- Plugins/GameFeatures/RadevaDimanaZombie/Source/RadevaDimanaZombieRuntime/Public/SteeringBehaviors.h
+++ Plugins/GameFeatures/RadevaDimanaZombie/Source/RadevaDimanaZombieRuntime/Public/SteeringBehaviors.h
@@ -3,26 +3,8 @@
 
 class ASurvivorPawn;
 
-//class Wander
-//{
-//public:
-//	FVector CalculateSteering(ASurvivorPawn* Pawn, float DeltaTime);
-//private:
-//	float WanderAngle = 0.f;
-//
-//	float CircleDistance = 300.f; 
-//	float CircleRadius = 150.f;  
-//	float WanderJitter = 2.f;
-//};
-
 class Seek
 {
 public:
 	FVector CalculateSteering(ASurvivorPawn* Pawn, FVector TargetLocation);
-};
-
-class Flee
-{
-public:
-	FVector CalculateSteering(ASurvivorPawn* Pawn, FVector ThreatLocation);
 };
